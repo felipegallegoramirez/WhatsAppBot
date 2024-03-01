4337498bfc672bd9b5bb38d7812d3c14234b114a
Felipe Gallego <felipegallego600@gmail.com>
2024-02-29 20:51:12 -0500

    Frontend-diagrams

--- src/Frontend/src/app/pages/diagrams/diagrams.component.ts
+++ src/Frontend/src/app/pages/diagrams/diagrams.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-diagrams',
@@ -7,6 +7,40 @@ import { Component } from '@angular/core';
   templateUrl: './diagrams.component.html',
   styleUrl: './diagrams.component.css'
 })
-export class DiagramsComponent {
+export class DiagramsComponent  implements OnInit{
+  ngOnInit(): void {
+    // Obtener el elemento canvas y el contexto 2D
+    var canvas = <HTMLCanvasElement>document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    if(ctx){
+    // Dibujar un rectángulo en el canvas
+    ctx.fillStyle = 'blue'; // Color del rectángulo
+    ctx.fillRect(50, 50, 100, 50); // x, y, width, height
+    }
+  }
+  state:boolean=false
+  menu(){
+    const menu=document.getElementById("menu")
+    const menuButton=document.getElementById("menu_button")
+    if(this.state){
+      menu?.classList.add("oculto")
+      menuButton?.classList.remove("oculto")
+    }else{
+      menuButton?.classList.add("oculto")
+      menu?.classList.remove("oculto")
+    }
+    this.state=!this.state
+  }
+
+  subMenu(menu:number){
+    const buttons=document.getElementsByClassName("menu_sup_button")
+    const subMenu=document.getElementsByClassName("option")
+    for(let i=0;i<3;i++){
+      buttons[i].classList.remove("select")
+      subMenu[i].classList.add("oculto")
+    }
+    buttons[menu].classList.add("select")
+    subMenu[menu].classList.remove("oculto")
+  }
 
 }
